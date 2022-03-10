24d2477fbee9da853310d65d137dd6b9caadec5d
Jackson Aquino Primo <jackson.primodev@gmail.com>
2022-03-10 12:26:34 -0300

    added url controller unit test to create and delete

--- src/modules/url/url.controller.spec.ts
+++ src/modules/url/url.controller.spec.ts
@@ -1,6 +1,5 @@
 import { CanActivate, HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreateUrlDto } from 'src/dtos/url.dto';
 import { LoggedGuard } from 'src/guards/logged.guard';
 import { TestUtil } from 'src/utils/test.util';
 import { TokenUtil } from 'src/utils/token.util';
@@ -50,4 +49,66 @@ describe('UrlController', () => {
   it('should be defined', () => {
     expect(urlController).toBeDefined();
   });
+  describe('create a url', ()=>{
+    it('should be return a exception case token is invalid', async ()=>{
+      mockTokenUtil.validateToken.mockImplementation(()=>{
+        throw new HttpException('token not valid', 401);
+      })
+      await urlController.create({authorization: "Bearer token"}, {
+        originalUrl: "example.com"
+      }).catch(e=>{
+        expect(e.message).toEqual('token not valid')
+      })
+    })
+    it('should be return a exception case token is expired', async ()=>{
+      mockTokenUtil.validateToken.mockImplementation(()=>{
+        throw new HttpException('token expired', 401);
+      })
+      await urlController.create({authorization: "Bearer token"}, {
+        originalUrl: "example.com"
+      }).catch(e=>{
+        expect(e.message).toEqual('token expired')
+      })
+    })
+    it('should be create a url', async ()=>{
+      const url = TestUtil.getUrlValidWithUser()
+      mockTokenUtil.validateToken.mockResolvedValue(null)
+      mockTokenUtil.decodeToken.mockResolvedValue({id: url.userId})
+      mockUrlService.create.mockResolvedValue(url)
+      const urlCreated = await urlController.create(
+        { authorization: "Bearer token" }, 
+        { originalUrl: url.originalUrl }
+      )
+      expect(urlCreated).toEqual(url)
+    })
+  })
+  describe('delete a url', ()=>{
+    it('should be return a exception case token is invalid', async ()=>{
+      mockTokenUtil.validateToken.mockImplementation(()=>{
+        throw new HttpException('token not valid', 401);
+      })
+      await urlController.delete({id: 'urlId'}, {authorization: "Bearer token"}).catch(e=>{
+        expect(e.message).toEqual('token not valid')
+      })
+    })
+    it('should be return a exception case token is expired', async ()=>{
+      mockTokenUtil.validateToken.mockImplementation(()=>{
+        throw new HttpException('token expired', 401);
+      })
+      await urlController.delete({id: 'urlId'}, {authorization: "Bearer token"}).catch(e=>{
+        expect(e.message).toEqual('token expired')
+      })
+    })
+    it('should be delete a url', async ()=>{
+      const url = TestUtil.getUrlValidWithUser()
+      mockTokenUtil.validateToken.mockResolvedValue(null)
+      mockTokenUtil.decodeToken.mockResolvedValue({id: url.userId})
+      mockUrlService.delete.mockResolvedValue(url)
+      const urlCreated = await urlController.delete(
+        { id: url.originalUrl },
+        { authorization: "Bearer token" }, 
+      )
+      expect(urlCreated).toEqual(url)
+    })
+  })
 });
