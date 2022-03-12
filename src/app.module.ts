2fe41108d637f0e6de3e029348a6a08f9e4c8ae5
Jackson Aquino Primo <jackson.primodev@gmail.com>
2022-03-12 19:06:48 -0300

    adjusted app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { TokenUtil } from './utils/token.util';
-import { CryptographyUtil } from './utils/cryptography.util';
 import { UrlModule } from './modules/url/url.module';
 
 @Module({
   imports: [AuthModule, UrlModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, TokenUtil, CryptographyUtil],
+  providers: [AppService],
 })
 export class AppModule {}
